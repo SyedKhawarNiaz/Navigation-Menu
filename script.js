92d30b01a0044dd2838a9ceb5638d007da0879bf
Syed Khawar <93276430+SyedKhawarNiaz@users.noreply.github.com>
2024-05-21 19:47:53 +0500

    Done

---
+++ script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector(".ri-menu-3-line");
+let back = document.querySelector(".ri-arrow-left-line");
+const tmline = gsap.timeline();
+menu.addEventListener("click", () => {
+  tmline.to(".sidebar", {
+    x: -360,
+    duration: 1,
+    ease: "power3.out",
+  });
+  tmline.from("ul li", {
+    x: -100,
+    opacity: 0,
+    duration: 1,
+    stagger: 1,
+  });
+  tmline.from(".ri-arrow-left-line", {
+    y: -100,
+    opacity: 0,
+    duration: 1,
+  });
+});
+back.addEventListener("click", () => {
+  gsap.to(".sidebar", {
+    x: 100,
+    duration: 1,
+  });
+});
