d377e21e923762f6c16b67eb1a55ced93b872feb
Syed Khawar <93276430+SyedKhawarNiaz@users.noreply.github.com>
2024-05-21 20:14:17 +0500

    Done

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 let menu = document.querySelector(".ri-menu-3-line");
 let back = document.querySelector(".ri-arrow-left-line");
 const tmline = gsap.timeline();
+tmline.to(".sidebar", {
+  x: -360,
+  duration: 0.8,
+  ease: "power3.out",
+});
+tmline.from("ul li", {
+  x: -100,
+  opacity: 0,
+  duration: 0.6,
+  stagger: 1,
+});
+tmline.from(".ri-arrow-left-line", {
+  y: -100,
+  opacity: 0,
+  duration: 1,
+});
+
+tmline.pause();
 menu.addEventListener("click", () => {
-  tmline.to(".sidebar", {
-    x: -360,
-    duration: 1,
-    ease: "power3.out",
-  });
-  tmline.from("ul li", {
-    x: -100,
-    opacity: 0,
-    duration:0.6,
-    stagger: 1,
-  });
-  tmline.from(".ri-arrow-left-line", {
-    y: -100,
-    opacity: 0,
-    duration: 1,
-  });
+  tmline.play();
 });
 back.addEventListener("click", () => {
-  gsap.to(".sidebar", {
-    x: 100,
-    duration: 1,
-  });
+  //   gsap.to(".sidebar", {
+  //     x: 100,
+  //     duration: 1,
+  //   });
+  tmline.reverse();
 });
